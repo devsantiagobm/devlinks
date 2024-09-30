b34ee94d1446fa2314547d979e0693e69305f927
Santiago Barrera Muñozm <sanseb290514@gmail.com>
2024-09-30 11:03:00 -0500

    fix: stoped seting cookies from server to client

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,17 +2,13 @@ const { request, response } = require("express")
 const helpers = require("/src/helpers/app.helper")
 const jwtHelpers = require("/src/helpers/jwt.helper")
 const authHelpers = require("/src/helpers/auth.helper")
-const { COOKIES_NAMES } = require("/src/constants")
 
 
 async function login(req = request, res = response) {
     try {
         req.user = await authHelpers.userExists(req, res);
         const token = jwtHelpers.generateToken({ user: req.user });
-
-        res.cookie(COOKIES_NAMES.auth_token, token);
-        res.cookie(COOKIES_NAMES.user_id, req.user._id.valueOf());
-        return res.status(200).json({ "message": "Log in completed" });
+        return res.status(200).json({ "message": "Log in completed", token, userID: req.user._id.valueOf() });
     } catch (error) {
         return helpers.returnError(error.message, res)
     }
@@ -21,12 +17,8 @@ async function login(req = request, res = response) {
 async function signUp(req = request, res = response) {
     try {
         req.user = await helpers.createUser(req, res);
-
         const token = jwtHelpers.generateToken({ user: req.user });
-        res.cookie(COOKIES_NAMES.auth_token, token);
-        res.cookie(COOKIES_NAMES.user_id, req.user._id.valueOf());
-
-        return res.status(200).json({ "message": "Sign up completed" });
+        return res.status(200).json({ "message": "Sign up completed", token, userID: req.user._id.valueOf()  });
     } catch (error) {
         return helpers.returnError(error.message, res)
     }
