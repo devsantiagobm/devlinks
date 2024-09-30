472873fab321126b665803445380513b5af6a0b7
Santiago Barrera Muñozm <sanseb290514@gmail.com>
2024-09-30 11:54:11 -0500

    fix: put the server url in images.domains allowed

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', "devlinks-api-nlgc.onrender.com"],
   },
   // https://stackoverflow.com/questions/65598753/cant-build-react-next-project-found-page-without-a-react-component-as-default
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
